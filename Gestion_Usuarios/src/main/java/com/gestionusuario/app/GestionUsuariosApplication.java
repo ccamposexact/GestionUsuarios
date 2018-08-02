8eaa5b0539d278b5f2b756bd948be458a401b40b
usuario <usuario@EXACT-001>
2018-08-02 16:08:12 -0500

    Commit Christian - Creacion de proyecto

---
+++ Gestion_Usuarios/src/main/java/com/gestionusuario/app/GestionUsuariosApplication.java
@@ -0,0 +1,12 @@
+package com.gestionusuario.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUsuariosApplication.class, args);
+	}
+}
